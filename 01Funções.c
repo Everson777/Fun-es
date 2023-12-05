c7a6dc7e3fe4e02ab53e3e0ee5712f5acc05fe17
Everson <143636906+Everson777@users.noreply.github.com>
2023-12-05 15:20:29 -0300

    Add files via upload

---
+++ 01Funções.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void cabecalho()
+{
+
+    printf("\n===================================\n");
+    printf("\t\tSenai");
+    printf("\n=====================================\n");
+}
+
+void Limpatela()
+{
+    fflush(stdin);
+    system("cls||clear");
+}
+
+int main()
+{
+
+    char nome[2000];
+    int idade;
+
+    cabecalho(); // Chamada da função.
+
+    printf("\nDigite seu nome:");
+    gets(nome);
+    Limpatela();
+
+    cabecalho();
+    printf("\nDigite uma idade:");
+    scanf("%d", &idade);
+    Limpatela();
+
+    cabecalho();
+
+    printf("\nNome:%s\n", nome);
+    printf("\nIdade:%d\n", idade);
+
+    return 0;
+}
